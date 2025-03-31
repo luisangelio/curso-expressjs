106a37e84aca732060ef181f0925f1ca7308a741
Luis <luisangeldev@gmail.com>
2025-03-31 12:19:46 -0500

    Add new endpoints
    - Install body-parser
    - Add get and post endpoints for testing
    - Add dynamic endpoint

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +14,49 @@ app.get('/', (req, res) => {
         <p>Esto es una aplicación node.js con express.js</p>
         <p>Corre en el puerto: ${PORT}</p>
     `);
+});
+
+app.get('/users/:id', (req, res) => { 
+    const userId = req.params.id;
+
+    res.send(`Mostrar información del usuario con ID: ${userId}`);
+});
+
+app.get("/search", (req, res) => { 
+    const terms = req.query.termino || 'No especificado';
+    const category = req.query.categoria || 'Todas';
+    
+    res.send(`
+        <h2>Resultados de búsqueda</h2>
+        <p>Término: ${terms}</p>
+        <p>Categoría: ${category}</p>
+    `);
+});
+
+app.post("/form", (req, res) => {
+    const name = req.body.nombre || 'Anónimo';
+    const email = req.body.email || 'No proporcionado';
+
+    res.json({
+        message: 'Datos recibidos',
+        data: {
+            name,
+            email,
+        }
+    });
+});
+
+app.post('/api/data', (req, res) => { 
+    const data = req.body;
+
+    if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({ error: 'No se recibieron datos' });
+    }
+
+    res.status(200).json({
+        message: 'Datos JSON recibidos',
+        data
+    });
 });
 
 app.listen(PORT, () => {
